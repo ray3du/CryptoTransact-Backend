64c86fb033bb601872f952355b602ba67bdec082
ray3du <edwinngugi149@gmail.com>
2021-10-23 09:43:59 +0300

    market data added

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
     res.status(200).json("{ msg: welcome home }");
 });
 
+
+router.post("/market/", (req, res) => {
+    const produce_data = () => {
+        const data = [
+            Math.round(Math.random() * 15) + 1, Math.round(Math.random() * 15) + 1
+         ]; 
+         return data;
+    };
+   
+   res.status(200).json({data: produce_data()});
+});
+
 module.exports = router;
